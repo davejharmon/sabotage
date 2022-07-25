b7f6047e7f105ce677fc40fa4dc6d3f1ad33b8f9
davejharmon <davejharmon@gmail.com>
2022-07-25 22:53:15 +1000

    basic prop passing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { LoginBar } from './Components/LoginBar';
 import { MainWindow } from './Components/MainWindow';
 import { StatusBar } from './Components/StatusBar';
+import { useState } from 'react';
 // eslint-disable-next-line no-unused-vars
 import firebase from './FirebaseConfig';
 import './App.css';
 
+const USER_LIST = ['Paul', 'John', 'George', 'Ringo'];
 function App() {
+  const [user, setUser] = useState(null);
   return (
     <div className="App">
-      <LoginBar />
+      <LoginBar user={user} setUser={setUser} userlist={USER_LIST} />
       <MainWindow />
       <StatusBar />
     </div>
--- src/Components/LoginBar.js
+++ src/Components/LoginBar.js
@@ -1,11 +1,32 @@
 import { Button } from './Button';
 import classes from './LoginBar.module.css';
-export const LoginBar = () => {
+export const LoginBar = props => {
+  const { user, setUser, userlist } = props;
+  console.log(userlist);
   return (
     <div className={classes.loginBar}>
-      <div>Logged in as:</div>
-      <div className={classes.loggedInAs}>Dave</div>
-      <Button type="Logout" />
+      {user ? (
+        <div>
+          <div>Logged in as:</div>
+          <div className={classes.loggedInAs}>{user}</div>
+          <Button type="Logout" />
+        </div>
+      ) : (
+        <div>
+          <div>Please log in...</div>
+          <select id="userlist" name="userlist">
+            {userlist ? (
+              userlist.map((username, i) => (
+                <option value={username} key={i}>
+                  {username}
+                </option>
+              ))
+            ) : (
+              <option>no users</option>
+            )}
+          </select>
+        </div>
+      )}
     </div>
   );
 };
